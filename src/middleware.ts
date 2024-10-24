88b05639aa338b38241059a6a9101c5509ca4c02
edburtnieks <edburtnieks@pm.me>
2024-10-24 14:40:33 +0300

    add rate limiter

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,37 @@
-import { defineMiddleware } from "astro:middleware";
+import { defineMiddleware, sequence } from "astro:middleware";
 import {
     deleteSessionTokenCookie,
     setSessionTokenCookie,
     validateSessionToken,
 } from "@lib/server/session";
+import { TokenBucket } from "@lib/server/rate-limit";
+
+const bucket = new TokenBucket(100, 1);
+
+const rateLimitMiddleware = defineMiddleware((context, next) => {
+    const clientIP = context.request.headers.get("X-Forwarded-For");
+    if (clientIP === null) {
+        return next();
+    }
+
+    let cost: number;
+    if (
+        context.request.method === "GET" ||
+        context.request.method === "OPTIONS"
+    ) {
+        cost = 1;
+    } else {
+        cost = 3;
+    }
+
+    if (!bucket.consume(clientIP, cost)) {
+        return new Response("Too many requests", {
+            status: 429,
+        });
+    }
+
+    return next();
+});
 
 const authMiddleware = defineMiddleware(async (context, next) => {
     const token = context.cookies.get("session")?.value ?? null;
@@ -31,4 +59,4 @@ const authMiddleware = defineMiddleware(async (context, next) => {
     return next();
 });
 
-export const onRequest = authMiddleware;
+export const onRequest = sequence(rateLimitMiddleware, authMiddleware);
