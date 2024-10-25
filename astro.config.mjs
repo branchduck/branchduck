9aadb74940a4cc4ff4c8112bdeadb108972982e9
edburtnieks <edburtnieks@pm.me>
2024-10-25 18:15:55 +0300

    replace rate limit with upstash rate limit

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,14 @@ export default defineConfig({
                 context: "server",
                 access: "secret",
             }),
+            UPSTASH_REDIS_REST_URL: envField.string({
+                context: "server",
+                access: "secret",
+            }),
+            UPSTASH_REDIS_REST_TOKEN: envField.string({
+                context: "server",
+                access: "secret",
+            }),
         },
     },
 });
